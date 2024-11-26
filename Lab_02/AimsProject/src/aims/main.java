4121ce7135536ed6a2afcd27d96a4e01ac75704d
Monsiertuna <macthanhbinhtn@gmail.com>
2024-11-26 11:31:01 +0700

    check

--- Lab_02/AimsProject/src/aims/main.java
+++ Lab_02/AimsProject/src/aims/main.java
@@ -3,24 +3,6 @@ package aims;
 public class main {
 
 	public static void main(String[] args) {
-	    // Tạo một giỏ hàng mới
-	    Cart anOrder = new Cart();
-
-	    // Tạo các đối tượng DVD và thêm chúng vào giỏ hàng
-	    DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-	    anOrder.addDigitalVideoDisc(dvd1);
-
-	    DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
-	    anOrder.addDigitalVideoDisc(dvd2);
-
-	    DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
-	    anOrder.addDigitalVideoDisc(dvd3);
-
-	    // In tổng chi phí của các mặt hàng trong giỏ hàng
-	    System.out.println("Total Cost is: ");
-	    System.out.println(anOrder.totalCost());
-	    anOrder.removeDigitalVideoDisc(dvd1);
-	    System.out.println(anOrder.totalCost());
 	    
 
 	}
