ec2eea7ee40f6dbc0a51dbe8d6992ffbfd580f91
Monsiertuna <macthanhbinhtn@gmail.com>
2024-12-02 23:37:06 +0700

    CD and Track

---
+++ Lab_02/AimsProject/src/hust/soict/dsai/aims/media/CD.java
@@ -0,0 +1,55 @@
+package hust.soict.dsai.aims.media;
+
+import java.util.List;
+
+public class CD extends Disc{
+	public CD(int id, String title, String category, float cost, String director, int length) {
+		super(id, title, category, cost, director, length);
+	}
+	private String artist;
+	private List<Track> tracks;
+	public String getArtist() {
+		return artist;
+	}
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	public List<Track> getTracks() {
+		return tracks;
+	}
+	public void setTracks(List<Track> tracks) {
+		this.tracks = tracks;
+	}
+	// Thêm bài mới
+	public void addTrack(Track track) {
+		if(!tracks.contains(track)) {
+			tracks.add(track);
+			System.out.println("Đã thêm vào " + getTitle());
+		}
+		else {
+			System.out.println("Đã có trong " + getTitle());
+		}
+	}
+	
+	//Xóa bài
+	public void removeTrack(Track track) {
+		if(tracks.remove(track)) {
+			System.out.println("Đã xóa khỏi " + getTitle());
+		}
+		else {
+			System.out.println("Không có trong " + getTitle());
+		}
+	}
+	
+	public void getLegth() {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
